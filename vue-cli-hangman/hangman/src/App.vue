265bc6c3d4c0e9d2ac6383238a60986a3b731c81
Kai Weluda <kaiweluda@gmail.com>
2023-05-26 11:13:11 +0200

    chore: initial commit for vue-cli

---
+++ vue-cli-hangman/hangman/src/App.vue
@@ -0,0 +1,14 @@
+<template></template>
+
+<script>
+export default {
+  name: "App",
+  components: {
+    KeyboardButtons,
+  },
+};
+</script>
+
+<style>
+#app {
+</style>
---
+++ vue-cli-hangman/hangman/src/components/KeyboardButtons.vue
@@ -0,0 +1,54 @@
+<template>
+  <footer
+    @click="
+      buttonClick($event);
+      checkForWinOrFail();
+    "
+  >
+    <template v-for="char in alphabet">
+      <button
+        v-if="goodChars.includes(char)"
+        disabled
+        class="button-keyboard"
+        :key="char"
+      >
+        {{ char }}
+      </button>
+      <button
+        v-else-if="badChars.includes(char)"
+        disabled
+        class="button-keyboard"
+        :key="char"
+      >
+        {{ char }}
+      </button>
+      <button
+        v-else-if="gameState === 'fail'"
+        disabled
+        class="button-keyboard"
+        :key="char"
+      >
+        {{ char }}
+      </button>
+      <button
+        v-else-if="gameState === 'success'"
+        disabled
+        class="button-keyboard"
+        :key="char"
+      >
+        {{ char }}
+      </button>
+      <button v-else class="button-keyboard" :key="char">{{ char }}</button>
+    </template>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      alphabet: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    };
+  },
+};
+</script>
